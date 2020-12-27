e732917ece74740f80edc02acfb601b0d97e9645
Vishal S <65108964+blog-a1@users.noreply.github.com>
2020-12-28 01:54:46 +0530

    Add files via upload

---
+++ problem solving/simple-array-sum.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int getsum(int a[],int n)
+{
+    int s=0;
+    for(int i=0;i<n;i++)    s+=a[i];
+    return s;
+}
+int main()
+{
+    int n;cin>>n;int a[n];
+    for(int i=0;i<n;i++)
+    {
+        cin>>a[i];
+    }
+    cout<<getsum(a,n)<<endl;return 0;
+}
