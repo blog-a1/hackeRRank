6e71cb118946fb7a293fd8cca504f4fdd522792c
vishal <65108964+blog-a1@users.noreply.github.com>
2021-05-02 09:31:35 +0530

    Add files via upload

---
+++ problem solving/correctness-and-the-loop-invariant.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n;cin>>n;int a[n];
+    for(int i=0;i<n;i++)
+        cin>>a[i];
+    for(int i=0;i<n;i++)
+    {
+        int temp=a[i],j=i;
+        while(j>0&&temp<a[j-1])
+        {
+            a[j]=a[j-1];
+            j-=1;
+        }
+        a[j]=temp;
+    }
+    for(int i=0;i<n;i++)
+        cout<<a[i]<<" ";
+    cout<<endl;
+    return 0;
+}
