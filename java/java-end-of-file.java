901a303e7692e46da0e5110ec03731ff4b7657f4
vishal <65108964+blog-a1@users.noreply.github.com>
2021-05-23 11:15:30 +0530

    Create java-end-of-file.java

---
+++ java/java-end-of-file.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner s=new Scanner(System.in); int i=1;
+        while(s.hasNext())
+        {
+            String str=s.nextLine();
+            System.out.println(i+" "+str);
+            i++;
+        }
+    }
+}
