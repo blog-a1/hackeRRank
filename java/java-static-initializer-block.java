d9b1cda33f7bcd14c2170f5c12feb1332a86de0e
vishal <65108964+blog-a1@users.noreply.github.com>
2021-05-24 10:56:20 +0530

    Create java-static-initializer-block.java

---
+++ java/java-static-initializer-block.java
@@ -0,0 +1,12 @@
+
+static Scanner s=new Scanner(System.in); 
+static int B=s.nextInt(); static int H=s.nextInt();  static boolean flag=true;
+static {
+    try{
+            if(B<=0||H<=0){
+            flag=false;
+            throw new Exception("Breadth and height must be positive");
+            } 
+    }catch(Exception e){System.out.println(e);}
+        
+}
