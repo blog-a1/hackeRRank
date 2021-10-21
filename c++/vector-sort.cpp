7d939608f30be7b956d707c4e3506f06b9978b55
vishal <65108964+blog-a1@users.noreply.github.com>
2021-10-21 08:45:24 +0530

    Create vector-sort.cpp

---
+++ c++/vector-sort.cpp
@@ -0,0 +1,23 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */  
+    vector<int>v;
+    int n,item;
+    cin>>n;
+    for(int i=0;i<n;i++) {
+        cin>>item;
+        v.push_back(item);
+    }
+    sort(v.begin(),v.end());
+    for(int i=0;i<n;i++) {
+        cout<<v[i]<<" ";
+    }
+    return 0;
+}
