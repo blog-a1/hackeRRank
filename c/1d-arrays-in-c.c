fed70f6cc855815b63883b4c230b671d317db153
vishal <65108964+blog-a1@users.noreply.github.com>
2021-11-12 09:09:13 +0530

    Create 1d-arrays-in-c.c

---
+++ c/1d-arrays-in-c.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */ 
+    int n;scanf("%d",&n);
+    int a[n],s=0;
+    for(int i=0;i<n;i++){
+        scanf("%d",&a[i]);s+=a[i];
+    }printf("%d",s);  
+    return 0;
+}
